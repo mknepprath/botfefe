c86e4bbe65f47fb69a605e701d2bd99f3071c272
Michael Knepprath <mknepprath@Ash.local>
2017-05-31 19:31:14 -0500

    Shuffle after .join()

--- ebooks.py
+++ ebooks.py
@@ -106,7 +106,7 @@ if __name__ == '__main__':
             random.shuffle(word)
             print word[:3]
             print random.shuffle(word[3:])
-            tweet_words[tweet_length - 1] = ''.join(word[:3]) + ''.join(random.shuffle(word[3:]))
+            tweet_words[tweet_length - 1] = ''.join(word[:3]) + random.shuffle(''.join(word[3:]))
         elif len(word) < 4:
             print 'Shuffled word too short. Aborting.'
             sys.exit()
