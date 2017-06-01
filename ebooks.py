fb75c708936ee484712a1b4162a1ac70f1f8eb34
Michael Knepprath <mknepprath@Ash.local>
2017-05-31 19:28:47 -0500

    Add colon instead of value to slice word

--- ebooks.py
+++ ebooks.py
@@ -105,8 +105,8 @@ if __name__ == '__main__':
         if len(word) > 5:
             random.shuffle(word)
             print word[:3]
-            print random.shuffle(word[3])
-            tweet_words[tweet_length - 1] = ''.join(word[:3]) + ''.join(random.shuffle(word[3:len(word)]))
+            print random.shuffle(word[3:])
+            tweet_words[tweet_length - 1] = ''.join(word[:3]) + ''.join(random.shuffle(word[3:]))
         elif len(word) < 4:
             print 'Shuffled word too short. Aborting.'
             sys.exit()
