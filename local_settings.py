b4360f2b21f8c3136e16b3ea8fb820eccd881bb9
Michael Knepprath <mknepprath@Ash.local>
2017-06-01 07:58:08 -0500

    Increase chance of tweeting

--- local_settings.py
+++ local_settings.py
@@ -3,6 +3,6 @@ Local Settings for botfefe.
 '''
 
 SOURCE_ACCOUNT = 'realdonaldtrump' #A list of comma-separated, quote-enclosed Twitter handles of account that you'll generate tweets based on. It should look like ["account1", "account2"]. If you want just one account, no comma needed.
-ODDS = 18 #How often do you want this to run? 1/8 times?
+ODDS = 4 #How often do you want this to run? 1/8 times?
 DEBUG = False #Set this to False to start Tweeting live
 TWEET_ACCOUNT = 'botfefe'
